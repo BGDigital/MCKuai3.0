e2892e7f97b9d4a9cb06698033723ab21863bf0f
kyly1982 <anloy_3633@sohu.com>
2015-06-28 20:12:29 +0800

    服务器信息

---
+++ app/src/main/java/com/mckuai/bean/GameServerInfo.java
@@ -0,0 +1,214 @@
+package com.mckuai.bean;
+
+/**
+ * Created by kyly on 2015/6/28.
+ */
+public class GameServerInfo {
+    private int gameId;
+    private int gameSize;
+    private int id;
+    private int isApprove;
+    private int isHuoDong;
+    private int isLock;
+    private int isNian;
+    private int isOpen;
+    private int isShowIp;
+    private int onlineNum;
+    private int peopleNum;
+    private int resCategroyId;
+    private int serverPort;
+    private int updateVersion;
+    private int userId;
+    private String resIp;
+    private String resIp2;
+    private String resIp3;
+    private String resVersion;
+    private String serverTag;
+    private String updateTime;
+    private String viewName;
+    private String whiteList;
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
+    public int getGameSize() {
+        return gameSize;
+    }
+
+    public void setGameSize(int gameSize) {
+        this.gameSize = gameSize;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIsApprove() {
+        return isApprove;
+    }
+
+    public void setIsApprove(int isApprove) {
+        this.isApprove = isApprove;
+    }
+
+    public int getIsHuoDong() {
+        return isHuoDong;
+    }
+
+    public void setIsHuoDong(int isHuoDong) {
+        this.isHuoDong = isHuoDong;
+    }
+
+    public int getIsLock() {
+        return isLock;
+    }
+
+    public void setIsLock(int isLock) {
+        this.isLock = isLock;
+    }
+
+    public int getIsNian() {
+        return isNian;
+    }
+
+    public void setIsNian(int isNian) {
+        this.isNian = isNian;
+    }
+
+    public int getIsOpen() {
+        return isOpen;
+    }
+
+    public void setIsOpen(int isOpen) {
+        this.isOpen = isOpen;
+    }
+
+    public int getIsShowIp() {
+        return isShowIp;
+    }
+
+    public void setIsShowIp(int isShowIp) {
+        this.isShowIp = isShowIp;
+    }
+
+    public int getOnlineNum() {
+        return onlineNum;
+    }
+
+    public void setOnlineNum(int onlineNum) {
+        this.onlineNum = onlineNum;
+    }
+
+    public int getPeopleNum() {
+        return peopleNum;
+    }
+
+    public void setPeopleNum(int peopleNum) {
+        this.peopleNum = peopleNum;
+    }
+
+    public int getResCategroyId() {
+        return resCategroyId;
+    }
+
+    public void setResCategroyId(int resCategroyId) {
+        this.resCategroyId = resCategroyId;
+    }
+
+    public int getServerPort() {
+        return serverPort;
+    }
+
+    public void setServerPort(int serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    public int getUpdateVersion() {
+        return updateVersion;
+    }
+
+    public void setUpdateVersion(int updateVersion) {
+        this.updateVersion = updateVersion;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getResIp() {
+        return resIp;
+    }
+
+    public void setResIp(String resIp) {
+        this.resIp = resIp;
+    }
+
+    public String getResIp2() {
+        return resIp2;
+    }
+
+    public void setResIp2(String resIp2) {
+        this.resIp2 = resIp2;
+    }
+
+    public String getResIp3() {
+        return resIp3;
+    }
+
+    public void setResIp3(String resIp3) {
+        this.resIp3 = resIp3;
+    }
+
+    public String getResVersion() {
+        return resVersion;
+    }
+
+    public void setResVersion(String resVersion) {
+        this.resVersion = resVersion;
+    }
+
+    public String getServerTag() {
+        return serverTag;
+    }
+
+    public void setServerTag(String serverTag) {
+        this.serverTag = serverTag;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getViewName() {
+        return viewName;
+    }
+
+    public void setViewName(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public String getWhiteList() {
+        return whiteList;
+    }
+
+    public void setWhiteList(String whiteList) {
+        this.whiteList = whiteList;
+    }
+}
