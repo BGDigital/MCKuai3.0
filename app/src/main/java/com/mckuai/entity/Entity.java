b11735372c2d58d4ac798cb7cd9dfc71f2f2c699
kyly1982 <anloy_3633@sohu.com>
2015-07-10 20:20:59 +0800

    读取游戏数据库

--- app/src/main/java/com/mckuai/entity/Entity.java
+++ app/src/main/java/com/mckuai/entity/Entity.java
@@ -5,26 +5,19 @@ import com.mckuai.until.Vector3f;
 import java.io.Serializable;
 
 public class Entity implements Serializable {
-
-	private Vector3f location = new Vector3f(0, 0, 0);
-
-	private float pitch;
-
-	private float yaw;
-
-	private Vector3f motion = new Vector3f(0, 0, 0);
-
-	private float fallDistance;
-
-	private short fire;
-
-	private short air = 300;
-
-	private boolean onGround = false;
-
-	private int typeId = 0;
+    private int typeId = 0;               //类型id:64为item drops
+	private Vector3f location = new Vector3f(0, 0, 0);   //此实体的当前坐标(三维,每个维度为float)
+    private Vector3f motion = new Vector3f(0, 0, 0);     //此实体在三个维度的运动速度(0,0,0为静止)
+    //以下两个数据用于存储实体的旋转度
+	private float pitch;             //俯仰(绕x轴的旋转)
+	private float yaw;               //航向(绕y轴的旋转)
+	private float fallDistance;    //下落高度，值越大在接触陆地时伤害越高
+	private short fire;                //火被扑灭时的刻度值，负值代表该实体可以在火中支撑多久
+	private short air = 300;       //该实体拥有的空气数量。在空气中为300,在水下会减少
+	private boolean onGround = false;                //是否在地面上
 
 	private Entity riding = null;
+
 
 	public Vector3f getLocation() {
 		return location;
