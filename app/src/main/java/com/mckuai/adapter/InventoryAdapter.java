94d593a05e4588b237413eddb958f0b4ee384286
kyly1982 <anloy_3633@sohu.com>
2015-07-16 03:58:54 +0800

    获取数据库中的角色信息

--- app/src/main/java/com/mckuai/adapter/InventoryAdapter.java
+++ app/src/main/java/com/mckuai/adapter/InventoryAdapter.java
@@ -1,21 +1,19 @@
 package com.mckuai.adapter;
 
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mckuai.InventorySlot;
 import com.mckuai.ItemStack;
-import com.mckuai.bean.ArticItem;
 import com.mckuai.entity.EntityItem;
 import com.mckuai.imc.R;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -23,24 +21,36 @@ import java.util.List;
  */
 public class InventoryAdapter extends RecyclerView.Adapter<InventoryAdapter.ViewHolder> {
 
-    private List<InventorySlot> inventorySlotArrayList;
-    private HashMap<Integer,Integer> selecteds = new HashMap<>(10);
-    private OnItemLongClickListener mListener;
-    private ArrayList<EntityItem> itemList;
+    private List<InventorySlot> inventorySlotArrayList;//这个是背包里的东西，最大数量为36
+    private ArrayList<EntityItem> itemList;//这个是所有的物品的列表,显示的就是它
+    private OnItemClickedListener mListener;
 
-    public interface OnItemLongClickListener{
-        public void onLongClick(InventorySlot inventorySlot);
+    public interface  OnItemClickedListener{
+        public void OnItemClicked(InventorySlot item);
     }
 
-    public void setOnItemLongClickListener(OnItemLongClickListener l){
-        this.mListener = l;
+    /**
+     * 设置item点击事件监听器
+     * @param listener item点击监听器
+     */
+    public void setOnItemClickedListener(OnItemClickedListener listener){
+        this.mListener = listener;
     }
 
+
+    /**
+     * 设置背包，背包中的物品将呈现出被选中的状态
+     * @param inventorySlotArrayList
+     */
     public void setInventorySlot(List<InventorySlot>inventorySlotArrayList){
         this.inventorySlotArrayList = inventorySlotArrayList;
         notifyDataSetChanged();
     }
 
+    /**
+     * 更新被修改过数量的插槽
+     * @param inventorySlot 被修改过的插槽
+     */
     public void updateInventory(InventorySlot inventorySlot){
         if (null != inventorySlot && null != inventorySlotArrayList && !inventorySlotArrayList.isEmpty()){
             for (InventorySlot item:inventorySlotArrayList){
@@ -49,16 +59,23 @@ public class InventoryAdapter extends RecyclerView.Adapter<InventoryAdapter.View
                     break;
                 }
             }
+            if (inventorySlot.getContents().getAmount() == 0){
+                inventorySlotArrayList.remove(inventorySlot);
+            }
             notifyDataSetChanged();
         }
     }
 
-    public HashMap<Integer,Integer> getSelectedItem(){
-        return  selecteds;
-    }
+    /**
+     * 获取修改后的背包
+     * @return
+     */
     public List<InventorySlot> getInventorySlots(){
         return  inventorySlotArrayList;
     }
+
+
+
 
     public InventoryAdapter(){
         super();
@@ -67,66 +84,103 @@ public class InventoryAdapter extends RecyclerView.Adapter<InventoryAdapter.View
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_game_article,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_game_article, parent, false);
 
         final ViewHolder holder = new ViewHolder(view);
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                InventorySlot item = (InventorySlot) holder.itemView.getTag();
-                if (null != item) {
-                    if (holder.iv_selected.getVisibility() == View.VISIBLE) {
-                        holder.iv_selected.setVisibility(View.INVISIBLE);
-                        selecteds.remove(item.getContents().getTypeId());
-                    } else {
-                        holder.iv_selected.setVisibility(View.VISIBLE);
-                        selecteds.put((int) item.getContents().getTypeId(), item.getContents().getAmount());
-                    }
+                EntityItem item = (EntityItem) holder.itemView.getTag();
+                //如果是已有的背包，直接取，否则够造一个
+                int index = getInventoryIndex(item);
+                InventorySlot inventorySlot;
+                if (0 > index){
+                   inventorySlot = createInventorySlot(item);
                 }
-
+                else {
+                    inventorySlot = inventorySlotArrayList.get(index);
+                }
+                if (null != mListener){
+                    mListener.OnItemClicked(inventorySlot);
+                }
             }
         });
-        view.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                InventorySlot item = (InventorySlot) holder.itemView.getTag();
-                if (null != item && null != mListener){
-                    mListener.onLongClick(item);
-                    return  true;
-                }
-                return false;
-            }
-        });
-        return holder;
+        return  holder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-//        ItemStack itemStack =  inventorySlotArrayList.get(position).getContents();
-//        holder.tv_name.setText("ID:" + itemStack.getTypeId() + ",数量:" +itemStack.getAmount());
-//        holder.itemView.setTag(inventorySlotArrayList.get(position));
         EntityItem item = itemList.get(position);
         if (null != item){
             holder.itemView.setTag(item);
             holder.tv_name.setText(item.getName());
-            if (isInInventory(item)){
-                holder.iv_selected.setVisibility(View.VISIBLE);
-            }
-            else {
+            int index = getInventoryIndex(item);
+            if (0 > index){
                 holder.iv_selected.setVisibility(View.INVISIBLE);
             }
+            else {
+                holder.iv_selected.setVisibility(View.VISIBLE);
+            }
+            holder.itemView.setTag(item);
         }
     }
 
-    private boolean isInInventory(EntityItem item){
+    /**
+     * 检查所给的物品是否已存在于背包中
+     * @param item
+     * @return
+     */
+    private int getInventoryIndex(EntityItem item){
         if (null != inventorySlotArrayList && !inventorySlotArrayList.isEmpty()){
-            for (InventorySlot slot:inventorySlotArrayList){
-                if (slot.getContents().getId() == item.getId()){
-                    return true;
+            for (int i = 0;i < inventorySlotArrayList.size();i++){
+                InventorySlot itemSlot = inventorySlotArrayList.get(i);
+                if (itemSlot.getContents().getId() == item.getId()){
+                    return i;
                 }
             }
         }
-        return false;
+        return -1;
+    }
+
+    /**
+     * 生成一个新的InventorySlot
+     * @param item
+     * @return
+     */
+    private InventorySlot createInventorySlot(EntityItem item){
+        if (null == inventorySlotArrayList){
+            inventorySlotArrayList = new ArrayList<>(10);
+        }
+        if (inventorySlotArrayList.size()  < 40){
+            ItemStack itemStack = new ItemStack(item.getId(),(short)255,0);
+            short index = 0;
+            for (short i = 9;i < 51;i++){
+                boolean result = false;
+                for (InventorySlot inventorySlot:inventorySlotArrayList){
+
+                    if (byte2Short(inventorySlot.getSlot())  == i){
+                        result = true;
+                        break;
+                    }
+                }
+                if (!result){
+                    index = i;
+                    break;
+                }
+            }
+            InventorySlot inventorySlot = new InventorySlot(short2Byte(index),itemStack);
+            inventorySlotArrayList.add(inventorySlot);
+           return inventorySlot;
+        }
+        return null;
+    }
+
+    private short byte2Short(byte b){
+        return (short)(b&0xFF);
+    }
+
+    private byte short2Byte(short s) {
+        return (byte)(s & 0xff);
     }
 
     @Override
