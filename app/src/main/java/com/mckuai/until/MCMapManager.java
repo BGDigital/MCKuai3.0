24e2221b71a56c6c593ecd2f0f744d2ed442b690
kyly1982 <anloy_3633@sohu.com>
2015-07-22 04:17:52 +0800

    优化结构，每次操作完成后均会关闭数据库

--- app/src/main/java/com/mckuai/until/MCMapManager.java
+++ app/src/main/java/com/mckuai/until/MCMapManager.java
@@ -7,13 +7,10 @@ import com.mckuai.WorldItem;
 import com.mckuai.bean.Map;
 import com.mckuai.io.db.DB;
 import com.mckuai.imc.MCkuai;
-import com.mckuai.io.db.Iterator;
 
 import org.zeroturnaround.zip.ZipUtil;
 
 import java.io.File;
-import java.lang.reflect.Array;
-import java.security.PrivateKey;
 import java.util.ArrayList;
 
 /**
@@ -28,85 +25,80 @@ public class MCMapManager {
 
     private static  final  String TAG = "MCMapManager";
 
-    private MCkuai application;
-
-//    private ArrayList<String> curMaps;                        //当前游戏中已经有的地图名称
-//    private ArrayList<String> curMapsDir;                   //当前游戏中地图的路径，与上面的一一对应
     private ArrayList<String> index;                             //这是所有的下载的地图的resid
     private ArrayList<Map> downloadMaps;                //已经下载了的
-    private ArrayList<Map> newDownloadMaps;         //新下载的东西存在这里
     private ArrayList<WorldItem> worldItemList;//游戏中的地图
     private DB db;
-    private File file;
+    private boolean isDBOpened = false;
     private String saveDir; //下载路径
+    private String worldRoot;
     private Gson gson ;
     private final  String mapIndex = "MAP_INDEX";
     private final  String mapDownloaded = "DOWNLOAD_MAP";
 
-    public void addDownloadMap(Map map){
-        if (null == newDownloadMaps){
-            newDownloadMaps = new ArrayList<Map>();
-        }
 
-        if (null == index){
-            getIndex();
-        }
-
-        for (String resId:index){
-            if (resId.equalsIgnoreCase(map.getResId())){
-                Log.w("addDownloadMap", "地图已存在!");
-                return;
+    public MCMapManager(){
+        gson = new Gson();
+        saveDir = MCkuai.getInstance().getMapDownloadDir();
+        worldRoot = MCkuai.getInstance().getGameProfileDir()+"minecraftWorlds/";
+        initDB();
+       /* File  file = new File(MCkuai.getInstance().getSDPath(),"games/com.mojang/minecraftWorlds");
+        worldItemList = new ArrayList<>();
+        if (file.exists()){
+            File[] fileList = file.listFiles();
+            for (File curFile:fileList){
+                worldItemList.add(new WorldItem(curFile));
             }
-        }
+        }*/
+    }
 
+    public void addDownloadMap(Map map){
+        if (null != index && !index.isEmpty()) {
+            for (String resId : index) {
+                if (resId.equalsIgnoreCase(map.getResId())) {
+                    Log.e("addDownloadMap", "地图已存在!");
+                    return;
+                }
+            }
+        }else {
+            index = new ArrayList<>();
+        }
         index.add(map.getResId());
-        newDownloadMaps.add(map);
+
         if (null == downloadMaps){
             downloadMaps = new ArrayList<>();
         }
         downloadMaps.add(map);
+        saveDB();
     }
 
     public boolean delDownloadMap(String mapId){
         if (null == mapId || null == index || index.isEmpty()){
             return false;
         }
-        for (String id:index){
-            if (id.equalsIgnoreCase(mapId)){
-                index.remove(id);  //删除索引
-                for (Map map:downloadMaps){
-                    if (map.getResId().equalsIgnoreCase(id)){
-                        downloadMaps.remove(map);//删除已下载地图中的记录
-                        break;
+        if (openDB()) {
+            for (String id : index) {
+                if (id.equalsIgnoreCase(mapId)) {
+                    for (Map map : downloadMaps) {
+                        if (map.getResId().equalsIgnoreCase(id)) {
+                            if (deleteMapfromDb(mapId)) {//删除数据库
+                                downloadMaps.remove(map);//删除已下载地图中的记录
+                                index.remove(id);//删除索引
+                                deleteMapfromDisk(map);//删除文件
+                                saveDB();
+                                closeDB();
+                                return true;
+                            }
+                        }
                     }
                 }
-                return deleteMapfromDb(mapId);  //删除数据库
             }
         }
-        return  false;
+        closeDB();
+        return false;
     }
 
-    private boolean deleteMapfromDb(String mapid){
-        if (!isReady()){
-            return  false;
-        }
-        try{
-            db.open();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-            byte data[] = db.get(mapid.getBytes());
-            if (null == data){
-                Log.e("deleteMapfromDb","map not exist");
-                return  false;
-            }
-            else {
-                db.delete(mapid.getBytes());
-                data = db.get(mapid.getBytes());
-                return  null == data;
-            }
-    }
+
 
     public ArrayList<Map> getDownloadMaps(){
         if (null != downloadMaps){
@@ -120,27 +112,63 @@ public class MCMapManager {
             }
         }
 
-        try {
-            db.open();
+        if (openDB()) {
+            for (String resid : index) {
+                byte data[] = db.get(resid.getBytes());
+                if (null == data) {
+                    closeDB();
+                    return null;
+                }
+                String maps = new String(data);
+
+                Map map = gson.fromJson(maps, Map.class);
+                if (null == downloadMaps) {
+                    downloadMaps = new ArrayList<>(index.size());
+                }
+                downloadMaps.add(map);
+            }
+            closeDB();
+            return downloadMaps;
         }
-        catch (Exception e){
-            e.printStackTrace();
+        return null;
+    }
+
+    public boolean saveDB(){
+        if (null == downloadMaps || downloadMaps.isEmpty() || null == index || index.isEmpty()){
+            return false;
         }
 
-        for (String resid:index){
-            byte data[]= db.get(resid.getBytes());
-            if (null == data){
-                return  null;
+        if (openDB()) {
+            String data = "";
+            for (String curmap : index) {
+                data += curmap + ",";
             }
-            String maps = new String(data);
+            if (data.length() > 1) {
+                data = data.substring(0, data.length() - 1);
+            }
+            db.put(mapIndex.getBytes(), data.getBytes());
 
-            Map map = gson.fromJson(maps,Map.class);
-            if (null == downloadMaps){
-                downloadMaps = new ArrayList<>(5);
+            for (Map map : downloadMaps) {
+                data = gson.toJson(map);
+                db.put(map.getResId().getBytes(), data.getBytes());
             }
-            downloadMaps.add(map);
+            closeDB();
+            return true;
         }
-        return  downloadMaps;
+        return false;
+    }
+
+    public boolean closeDB(){
+        if (isDBOpened && null != db) {
+            try {
+                db.close();
+                isDBOpened = false;
+            } catch (Exception e) {
+                Log.w("closeDB", e.getLocalizedMessage());
+                e.printStackTrace();
+            }
+        }
+        return !isDBOpened;
     }
 
     /**
@@ -158,7 +186,7 @@ public class MCMapManager {
         }
         //还未取出地图
         worldItemList = new ArrayList<>();
-        File[] files = new File(application.getGameProfileDir()+"minecraftWorlds/").listFiles();
+        File[] files = new File(worldRoot).listFiles();
         if (null == files || 0 == files.length){
             return  null;
         }
@@ -172,15 +200,15 @@ public class MCMapManager {
         return  dirs;
     }
 
-    private long getLastPlayTime(String path){
+/*    private long getLastPlayTime(String path){
         path = path + "/level.dat";
         File file = new File(path);
         long time = file.lastModified();
         file = null;
         return time;
-    }
+    }*/
 
-    private void insertNewGameMap(WorldItem world){
+ /*   private void insertNewGameMap(WorldItem world){
         for (int i = 0; i < worldItemList.size();i++){
             if (worldItemList.get(i).lastPlayTime > world.lastPlayTime){
                 worldItemList.add(i,world);
@@ -188,14 +216,14 @@ public class MCMapManager {
             }
         }
         worldItemList.add(world);
-    }
+    }*/
 
     /**
      * 获取当前正在使用的地图目录
      * 从游戏存档目录中获取最近被修改过的地图,均获取不到则返回空
      * @return
      */
-    public String getCurrentMapDir(){
+ /*   public String getCurrentMapDir(){
 
         if (null == worldItemList){
             getCurrentMapDirList();
@@ -206,14 +234,14 @@ public class MCMapManager {
         else {
             return  null;
         }
-    }
+    }*/
 
     /**
      * 获取当前的游戏地图的名称
      * 从当前正在使用的游戏存档中获取
      * @return
      */
-    public String getCurrentMapName(){
+    /*public String getCurrentMapName(){
         if (null == worldItemList){
             getCurrentMapDirList();
         }
@@ -225,7 +253,7 @@ public class MCMapManager {
             return null;
         }
 
-    }
+    }*/
 
     /**
      * 获取指定目录下的level.dat文件中读取地图名称
@@ -240,7 +268,7 @@ public class MCMapManager {
 
     public void importMap(String mapFileName){
         File src = new File(mapFileName);
-        File dst = new File(application.getGameProfileDir()+"minecraftWorlds/");
+        File dst = new File(worldRoot);
         if (!dst.exists()){
             Log.e(TAG,"目标目录不存在，可能是游戏未安装");
             return;
@@ -258,10 +286,10 @@ public class MCMapManager {
      * @param dstFileDir 导出到的目录
      */
     public void exportMap(String mapName,String dstFileDir){
-        File src = new File(MCkuai.getInstance().getGameProfileDir()+"minecraftWorlds/"+mapName);
+        File src = new File(worldRoot,mapName);
         File dst = new File(dstFileDir,mapName +".zip");
-        Log.e(TAG,"源路径："+src.getPath());
-        Log.e(TAG,"目标路径："+dst.getPath());
+        Log.e(TAG, "源路径：" + src.getPath());
+        Log.e(TAG, "目标路径：" + dst.getPath());
 
         if (src.exists() && src.isDirectory()){
             if (dst.exists()){
@@ -274,132 +302,72 @@ public class MCMapManager {
         }
     }
 
-    public MCMapManager(){
-        gson = new Gson();
-        application = MCkuai.getInstance();
-        saveDir = application.getMapDownloadDir();
-        initDB();
-        File  file = new File(MCkuai.getInstance().getSDPath(),"games/com.mojang/minecraftWorlds");
-        worldItemList = new ArrayList<>();
-        if (file.exists()){
-            File[] fileList = file.listFiles();
-            for (File curFile:fileList){
-                worldItemList.add(new WorldItem(curFile));
-            }
-        }
-    }
-
-    public boolean isReady(){
-        return null != db;
-    }
-
-
     private void initDB(){
         Log.w("initDB", "file:" + saveDir);
-        file = new File(saveDir);
+        File file = new File(saveDir);
         if (!file.exists()){
             file.mkdirs();
         }
         db = new DB(file);
         db.open();
-        if (!file.exists()){
-            Log.w("initDB","error:file not exist");
-        }
-        else{
-            Log.w("initDB","get file");
-        }
+        isDBOpened =true;
+
         getIndex();
-        downloadMaps = getDownloadMaps();
+        getDownloadMaps();
     }
 
-    public void saveDB(){
-        if (null != newDownloadMaps && !newDownloadMaps.isEmpty()){
-            if (!isReady()){
-                return;
+    private boolean deleteMapfromDb(String mapid){
+        if (openDB()) {
+            byte data[] = db.get(mapid.getBytes());
+            if (null == data) {
+                Log.e("deleteMapfromDb", "map not exist");
+                return false;
+            } else {
+                db.delete(mapid.getBytes());
+                data = db.get(mapid.getBytes());
+                return null == data;
             }
         }
-
-        try {
-            saveIndex();
-            saveNewDownload();
-        }
-        catch (Exception e){
-
-        }
+        return false;
     }
 
+    private boolean deleteMapfromDisk(Map map){
+        File file = new File(MCkuai.getInstance().getMapDownloadDir(),map.getFileName());
+        if (null != file && file.exists() && file.isFile()){
+            return  file.delete();
+        }
+        return  false;
+    }
 
-    public void closeDB(){
-        saveDB();
-        try{
-            db.close();
+    private boolean openDB(){
+        if (!isDBOpened && null != db){
+            try{
+                db.open();
+                isDBOpened = true;
+            }catch (Exception e){
+                e.printStackTrace();
+                isDBOpened = false;
+            }
         }
-        catch (Exception e){
-            Log.w("closeDB", e.getLocalizedMessage());
-        }
+        return isDBOpened;
     }
 
     private boolean getIndex(){
-        if (!isReady()){
-            Log.e(TAG, "getIndex: false,open db false!");
-            return false;
-        }
-
-        if (null == index){
+        if (openDB()) {
             index = new ArrayList<>();
-        }
-
-        byte data[] = db.get(mapIndex.getBytes());
-        if (null != data){
-            String mapindex = new String(data);
-            String arr[] = mapindex.split(",");
-            for (int i = 0;i <arr.length;i++){
-                index.add(arr[i]);
+            byte data[] = db.get(mapIndex.getBytes());
+            if (null != data) {
+                String mapindex = new String(data);
+                String arr[] = mapindex.split(",");
+                for (int i = 0; i < arr.length; i++) {
+                    index.add(arr[i]);
+                }
+                return true;
+            } else {
+                Log.e(TAG, "getDownloadMapsIndex: false,get null");
+                return false;
             }
-            return  true;
         }
-        else {
-            Log.e(TAG,"getDownloadMapsIndex: false,get null");
-            return  false;
-        }
+        return false;
     }
-
-    private void saveIndex(){
-        if (!isReady()){
-            return;
-        }
-
-        String data = "";
-        for (String curmap:index){
-            data += curmap + ",";
-        }
-        if (data.length() > 1) {
-            data = data.substring(0, data.length() - 1);
-        }
-
-        db.put(mapIndex.getBytes(), data.getBytes());
-    }
-
-    private void saveNewDownload(){
-        if (null == newDownloadMaps || newDownloadMaps.isEmpty()){
-            return;
-        }
-
-        if (!isReady()){
-            Log.e(TAG,"saveNewDownload: false,open db false!");
-            return;
-        }
-
-        for (Map map:newDownloadMaps){
-            String data = gson.toJson(map);
-            db.put(map.getResId().getBytes(),data.getBytes());
-        }
-
-        newDownloadMaps.clear();
-
-    }
-
-
-
-
 }
