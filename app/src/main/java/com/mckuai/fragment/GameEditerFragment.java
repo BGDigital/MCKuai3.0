58de2cd08b2f9bdf46f16c9f3f40a7a1b82a3782
kyly1982 <anloy_3633@sohu.com>
2015-08-14 04:40:40 +0800

    游戏多版本支持

--- app/src/main/java/com/mckuai/fragment/GameEditerFragment.java
+++ app/src/main/java/com/mckuai/fragment/GameEditerFragment.java
@@ -61,18 +61,17 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
     private String time;//白天黑夜
     private String viewName; //地图名字
     private boolean thirdPerson = false; //是否启用第三人称
-    private ArrayList<WorldItem> worldItems;
+    private ArrayList<WorldItem> worldItems;//世界
+    private ArrayList<GameItem> gameItems;//游戏版本
     private int inventoryTypeCount;         //背包中物品种类数
     private int curWorldIndex;//当前显示的世界的索引
-    private Integer[] res_Map = {R.drawable.background_map_0,R.drawable.background_map_1,R.drawable.background_map_2,R.drawable.background_map_3,R.drawable.background_map_4,R.drawable.background_map_5,R.drawable.background_map_6,R.drawable.background_map_7,R.drawable.background_map_8,R.drawable.background_map_9};
-//    private int res_Map_index = 0;
+    private Integer[] res_Map = {R.drawable.background_map_0, R.drawable.background_map_1, R.drawable.background_map_2, R.drawable.background_map_3, R.drawable.background_map_4, R.drawable.background_map_5, R.drawable.background_map_6, R.drawable.background_map_7, R.drawable.background_map_8, R.drawable.background_map_9};
 
     private ThinDownloadManager mDlManager;
 
     private boolean isShowUninstallAlert = true;
     private boolean isGameInstalled = false;
     private boolean isGameRunning = false;
-    private boolean isGameVersionSupport = false;
     private boolean isDownloadGame = false;
 
     private WorldAdapter adapter;
@@ -109,7 +108,7 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
             gameEditer = new MCWorldUtil(new MCWorldUtil.OnWorldLoadListener() {
                 @Override
                 public void OnComplete(ArrayList<WorldItem> worldItems, boolean isThirdView) {
-                    Log.e(TAG, "地图数目：" + (null == worldItems ? 0: worldItems.size()));
+                    Log.e(TAG, "地图数目：" + (null == worldItems ? 0 : worldItems.size()));
                     setData(worldItems, isThirdView);
                 }
             }, false);
@@ -119,7 +118,7 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
 
     @Override
     public void onDestroy() {
-        if (null!=mDlManager){
+        if (null != mDlManager) {
             mDlManager.cancelAll();
             mDlManager.release();
         }
@@ -133,15 +132,15 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
         tv_packageItemCount = (TextView) view.findViewById(R.id.tv_curItemCount);
         tv_thirdView = (TextView) view.findViewById(R.id.tv_curView);
         tv_mapName = (TextView) view.findViewById(R.id.tv_mapName);
-        tv_notification = (TextView)view.findViewById(R.id.tv_notificationTitle);
+        tv_notification = (TextView) view.findViewById(R.id.tv_notificationTitle);
         iv_map = (ImageView) view.findViewById(R.id.iv_map);
         iv_gameMode = (ImageView) view.findViewById(R.id.iv_gameMode);
         iv_gameTime = (ImageView) view.findViewById(R.id.iv_gameTime);
         iv_thirdView = (ImageView) view.findViewById(R.id.iv_thirdView);
         iv_packageItem = (ImageView) view.findViewById(R.id.iv_gamePackage);
         lv_mapList = (ListView) view.findViewById(R.id.lv_mapList);
-        rl_notification = (RelativeLayout)view.findViewById(R.id.rl_notificationDownloadProgress);
-        progressBar = (ProgressBar)view.findViewById(R.id.pb_downloadProgress);
+        rl_notification = (RelativeLayout) view.findViewById(R.id.rl_notificationDownloadProgress);
+        progressBar = (ProgressBar) view.findViewById(R.id.pb_downloadProgress);
 
         view.findViewById(R.id.rl_gameMode).setOnClickListener(this);
         view.findViewById(R.id.rl_gameTime).setOnClickListener(this);
@@ -160,8 +159,7 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
         if (null != worldList && !worldList.isEmpty()) {
             curWorldIndex = 0;
             getWorldInfo();
-        }
-        else {
+        } else {
             showNotification(2, "提示：未检测到地图，点击地图获取更多精彩地图！", R.id.fl_root);
         }
     }
@@ -294,16 +292,21 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
     }
 
     private void detectionGameInfo() {
-        if (MCkuai.getInstance().fragmentIndex != 0){
+        if (MCkuai.getInstance().fragmentIndex != 0) {
             return;
         }
-        ArrayList<GameItem> games = GameUntil.detectionGame(getActivity());
+        final ArrayList<GameItem> games = GameUntil.detectionGame(getActivity());
         isGameInstalled = GameUntil.detectionIsGameInstalled(getActivity());
-        if (null != games && !games.isEmpty()){
+        if (null != games && !games.isEmpty()) {
             isGameInstalled = true;
+            for (GameItem game : games) {
+                if (game.isRunning()) {
+                    isGameRunning = true;
+                    break;
+                }
+            }
         }
         if (isGameInstalled) {
-            isGameRunning = GameUntil.detectionIsGameRunning(getActivity());
             if (isGameRunning) {
                 showAlert("警告", "检测到我的世界正在运行,此时的修改不会生效,是否结束游戏?", new View.OnClickListener() {
                     @Override
@@ -313,21 +316,11 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
                     @Override
                     public void onClick(View v) {
                         isGameRunning = !GameUntil.killGameTask(getActivity());
+                        isGameRunning = !GameUntil.killGameTask(getActivity(), games);
                     }
                 });
             }
-//            int version = OptionUntil.getGameVersion_minor();
-
-            int version = GameUntil.detectionGameVersion(getActivity());
-            if (version < 9 || version > 10){
-                isGameVersionSupport = false;
-                showDownloadGame(false);
-            }
-            else {
-                isGameVersionSupport = true;
-            }
         } else {
-            //showMessage("警告", "你还未安装游戏，不能修改游戏内容！");
             showDownloadGame(false);
             return;
         }
@@ -355,32 +348,31 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
             return;
         }
 
-        if (!isGameInstalled && rl_notification.getVisibility() != View.VISIBLE){
+        if (!isGameInstalled && rl_notification.getVisibility() != View.VISIBLE) {
             showDownloadGame(true);
             return;
         }
         switch (v.getId()) {
             case R.id.rl_notificationDownloadProgress:
                 //安装游戏
-                MobclickAgent.onEvent(getActivity(),"installGame");
+                MobclickAgent.onEvent(getActivity(), "installGame");
                 String file = (String) v.getTag();
-                if (null != file){
-                    if (file.equals("false")){
+                if (null != file) {
+                    if (file.equals("false")) {
                         rl_notification.setVisibility(View.INVISIBLE);
-                    }
-                    else {
+                    } else {
                         installGame((String) v.getTag());
                     }
                 }
                 break;
             case R.id.rl_gameMode:
                 //修改游戏模式
-                MobclickAgent.onEvent(getActivity(),"switchGameMode");
-                if (rl_notification.getVisibility() == View.VISIBLE){
+                MobclickAgent.onEvent(getActivity(), "switchGameMode");
+                if (rl_notification.getVisibility() == View.VISIBLE) {
                     showNotification(3, "正在下载游戏，请稍候！", R.id.fl_root);
                     return;
                 }
-                if (!checkGameVersion()){
+                if (!checkGameInstall()) {
                     return;
                 }
                 if (null != worldItems && worldItems.get(curWorldIndex).getLevel() != null) {
@@ -391,12 +383,12 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
                 break;
             case R.id.rl_gameTime:
                 //切换日夜
-                MobclickAgent.onEvent(getActivity(),"switchTime");
-                if (rl_notification.getVisibility() == View.VISIBLE){
+                MobclickAgent.onEvent(getActivity(), "switchTime");
+                if (rl_notification.getVisibility() == View.VISIBLE) {
                     showNotification(3, "正在下载游戏，请稍候！", R.id.fl_root);
                     return;
                 }
-                if (!checkGameVersion()){
+                if (!checkGameInstall()) {
                     return;
                 }
                 if (null != worldItems && worldItems.get(curWorldIndex).getLevel() != null) {
@@ -407,8 +399,8 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
 
                 break;
             case R.id.rl_thirdView:
-                MobclickAgent.onEvent(getActivity(),"switchView");
-                if (rl_notification.getVisibility() == View.VISIBLE){
+                MobclickAgent.onEvent(getActivity(), "switchView");
+                if (rl_notification.getVisibility() == View.VISIBLE) {
                     showNotification(3, "正在下载游戏，请稍候！", R.id.fl_root);
                     return;
                 }
@@ -422,25 +414,24 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
                 break;
             case R.id.rl_gamePackage:
                 //修改背包
-                MobclickAgent.onEvent(getActivity(),"showPackage");
-                if (rl_notification.getVisibility() == View.VISIBLE){
+                MobclickAgent.onEvent(getActivity(), "showPackage");
+                if (rl_notification.getVisibility() == View.VISIBLE) {
                     showNotification(3, "正在下载游戏，请稍候！", R.id.fl_root);
                     return;
                 }
-                if (!checkGameVersion()){
+                if (!checkGameInstall()) {
                     return;
                 }
                 if (null != worldItems && null != worldItems.get(curWorldIndex)) {
                     changePackageItem();
-                }
-                else {
+                } else {
                     showNotification(3, "提示：未检测到地图，点击地图获取更多精彩地图！", R.id.fl_root);
                 }
                 break;
             case R.id.btn_startGame:
                 //运行游戏
                 MobclickAgent.onEvent(getActivity(), "startGame_tool");
-                if (rl_notification.getVisibility() == View.VISIBLE){
+                if (rl_notification.getVisibility() == View.VISIBLE) {
                     showNotification(3, "正在下载游戏，请稍候！", R.id.fl_root);
                     return;
                 }
@@ -450,12 +441,12 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
                 break;
             case R.id.btn_selectMap:
                 //选择地图
-                MobclickAgent.onEvent(getActivity(),"selectMap");
-                if (rl_notification.getVisibility() == View.VISIBLE){
+                MobclickAgent.onEvent(getActivity(), "selectMap");
+                if (rl_notification.getVisibility() == View.VISIBLE) {
                     showNotification(3, "正在下载游戏，请稍候！", R.id.fl_root);
                     return;
                 }
-                if (!checkGameVersion()){
+                if (!checkGameInstall()) {
                     return;
                 }
                 if (null != worldItems && !worldItems.isEmpty()) {
@@ -471,11 +462,11 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
         }
     }
 
-    private boolean checkGameVersion(){
-        if (!isGameVersionSupport ){
+    private boolean checkGameInstall() {
+        if (!isGameInstalled) {
             showDownloadGame(true);
         }
-        return isGameVersionSupport;
+        return isGameInstalled;
     }
 
     @Override
@@ -485,12 +476,12 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
         getWorldInfo();
     }
 
-    private void showDownloadGame(boolean showAlert){
-        if (!showAlert && (isDownloadGame || !isShowUninstallAlert)){
+    private void showDownloadGame(boolean showAlert) {
+        if (!showAlert && (isDownloadGame || !isShowUninstallAlert)) {
             return;
         }
         isShowUninstallAlert = false;
-        MobclickAgent.onEvent(getActivity(),"showDownloadGame");
+        MobclickAgent.onEvent(getActivity(), "showDownloadGame");
         showAlert("提示", "为了更好的体验游戏，请下载《我的世界0.10.5》\n是否立即下载游戏？", new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -499,11 +490,11 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
         }, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MobclickAgent.onEvent(getActivity(),"downloadGame");
+                MobclickAgent.onEvent(getActivity(), "downloadGame");
                 String url = "http://softdown.mckuai.com:8081/wodeshijie_v_0_10_5.apk";
                 final String downloadDir = MCkuai.getInstance().getMapDownloadDir() + url.substring(url.lastIndexOf("/") + 1, url.length());
                 File file = new File(downloadDir);
-                if (null != file && (!file.exists() || !file.isDirectory())){
+                if (null != file && (!file.exists() || !file.isDirectory())) {
                     file.mkdirs();
                 }
                 mDlManager = new ThinDownloadManager(1);
@@ -534,7 +525,7 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
         });
     }
 
-    private void installGame(String file){
+    private void installGame(String file) {
         isDownloadGame = false;
         if (null != file && file.length() > 10) {
             File apkFile = new File(file);
@@ -549,7 +540,7 @@ public class GameEditerFragment extends BaseFragment implements View.OnClickList
 
     @Override
     public boolean onBackKeyPressed() {
-        if(lv_mapList.getVisibility() == View.VISIBLE){
+        if (lv_mapList.getVisibility() == View.VISIBLE) {
             lv_mapList.setVisibility(View.GONE);
             return true;
         }
