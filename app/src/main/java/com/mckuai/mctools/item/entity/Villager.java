b353a454c01801a77468ffe356ad5ecd94cf9df4
kyly1982 <anloy_3633@sohu.com>
2015-08-18 19:43:47 +0800

    村民

--- app/src/main/java/com/mckuai/mctools/item/entity/Villager.java
+++ app/src/main/java/com/mckuai/mctools/item/entity/Villager.java
@@ -5,7 +5,7 @@ import com.mckuai.mctools.item.entity.Animal;
 /**
  * Created by kyly on 2015/7/10.
  */
-public class Villa extends Animal {
+public class Villager extends Animal {
     public int getMaxHealth()
     {
         return 10;
